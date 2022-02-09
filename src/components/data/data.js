d467ce820a6fd9c5d01ebf481addc38268d1421a
AzatBaisynov <azat.baisynov@gmail.com>
2022-02-09 14:38:45 +0600

    prodccecc

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -8,7 +8,7 @@ export const data =
     }
 
 const DEV = "http://localhost:8080"
-const PROD = "http://192.168.1.67:8080"
+const PROD = "http://dms.ccecc.kz:8080/"
 
 export const address = {
     use : PROD
